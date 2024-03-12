1a398863f43539bd1a61048db27255e4ae2cdeb1
Irtaza <arsal.h@yahoo.com>
2024-03-13 02:01:46 +0500

    student crud done

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -19,36 +19,27 @@ class StudentController extends Controller
     {
         return $this->student->all();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+ 
     public function store(Request $request)
     {
-        //
+        return $this->student->create($request->all());
     }
-
-    /**
-     * Display the specified resource.
-     */
+ 
     public function show(string $id)
     {
-        //
+        $student = $this->student->find($id);  
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
+ 
     public function update(Request $request, string $id)
     {
-        //
+        $student = $this->student->find($id);
+        $student->update($request->all());
+        return $student;
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+ 
     public function destroy(string $id)
     {
-        //
+        $student = $this->student->find($id);
+        return $student->delete();
     }
 }
